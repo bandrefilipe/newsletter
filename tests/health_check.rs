b73ce21f7e1b364f610cceba78f76260789b822a
bandrefilipe <b.andrefilipe@gmail.com>
2022-03-20 03:29:08 -0300

    test: validation tests for the subscription api

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,6 +3,8 @@ use std::net::TcpListener;
 
 const LOCALHOST: &str = "127.0.0.1";
 
+/// Spins up an instance of our application
+/// and returns its address (i.e. http://localhost:0)
 fn spawn_app() -> String {
     let listener =
         TcpListener::bind(format!("{LOCALHOST}:0")).expect("Failed to bind a random port.");
@@ -30,4 +32,55 @@ async fn health_check_works() {
     // Assert
     assert_eq!(response.status(), StatusCode::OK);
     assert_eq!(response.content_length(), Some(0));
+}
+
+#[tokio::test]
+async fn subscriptions_returns_200_for_valid_form_data() {
+    // Arrange
+    let base_url = spawn_app();
+    let client = reqwest::Client::new();
+    let request_body = "name=Andr%C3%A9%20Filipe&email=b.andrefilipe%40gmail.com";
+
+    // Act
+    let response = client
+        .post(format!("{base_url}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn subscriptions_returns_400_when_data_is_missing() {
+    // Arrange
+    let base_url = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=Andr%C3%A9%20Filipe", "missing the email"),
+        ("email=b.andrefilipe%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, invalid_reason) in test_cases {
+        // Act
+        let response = client
+            .post(format!("{base_url}/subscriptions"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            response.status(),
+            StatusCode::BAD_REQUEST,
+            "POST /subscriptions dit not fail with 400 Bad Request when the payload was {}.",
+            invalid_reason
+        );
+    }
 }
