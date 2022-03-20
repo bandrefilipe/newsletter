379ac9b956afa386f090c77ce1d7ad9d34411f80
bandrefilipe <b.andrefilipe@gmail.com>
2022-03-20 03:39:14 -0300

    feat: add a subsciptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,25 @@ async fn health() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct SubscriptionForm {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(form: web::Form<SubscriptionForm>) -> impl Responder {
+    dbg!(&form.name, &form.email);
+    HttpResponse::Ok()
+}
+
 pub fn run_server(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health", web::get().to(health)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
